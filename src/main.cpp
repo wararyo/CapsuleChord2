7af2d4c7d6e599cd7d455b97c5da667bfb792367
wararyo <wararyo@gmail.com>
2024-08-18 21:06:20 +0900

    main内の現在使用していない記述を一時的に削除
    BLE関連の記述とシーン関連の記述を削除

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,11 @@
 #include "Context.h"
 #include "Output/MidiOutput.h"
 #include "Tempo.h"
-
-#define DEVICE_NAME "CapsuleChord 2"
 #include "ChordPipeline.h"
 
 #define GPIO_NUM_BACK GPIO_NUM_7
 #define GPIO_NUM_HOME GPIO_NUM_5
 #define GPIO_NUM_MENU GPIO_NUM_8
-
-bool seventh = false;
-int option = 0;
-
-float expression = 64;
 
 unsigned long lastLoopMillis = 0;
 
@@ -51,70 +44,6 @@ Settings settings(si{
   new SettingItemEnum("SustainBehavior",{"Normal","Trigger"},0),
   new SettingItemEnum("Brightness", {"Bright","Normal","Dark"},1)
 });
-
-enum Scene : uint8_t {
-  Connection,
-  Play,
-  FunctionMenu,
-  length
-};
-
-Scene currentScene = Scene::length; //Specify a value other than Connection in order to draw correctly at the first changeScene()
-Scene requiredToChangeScene;
-
-void changeScene(Scene scene) {
-  //To draw at the end of loop(), set flag
-  requiredToChangeScene = scene;
-}
-
-//Used only in setup() and the end of loop() Do not touch it anywhere else!
-void _changeScene_raw() {
-  M5.Lcd.clear();
-
-  //終了処理
-  switch(currentScene) {
-  }
-
-  //開始処理
-  switch(requiredToChangeScene) {
-    case Scene::Connection:
-      M5.Lcd.setTextFont(1);
-      if(SD.exists("/capsulechord/splash.jpg")){
-        // M5.Display.drawJpgFile(SD, "/capsulechord/splash.jpg", 0, 0);
-      }
-      else {
-        M5.Lcd.setCursor(0, 48);
-        M5.Lcd.setTextSize(4);
-        M5.Lcd.println("CapsuleChord2");
-        M5.Lcd.setTextSize(2);
-        M5.Lcd.println("BLE MIDI Chordpad Device\n");
-        M5.Lcd.setTextSize(1);
-        M5.Lcd.println("Advertising...");
-        M5.Lcd.println("Press the button A to power off.");
-      }
-    break;
-    case Scene::Play:
-      M5.Lcd.setCursor(0,0);
-      M5.Lcd.setTextSize(2);
-      M5.Lcd.println(scale->toString());
-    break;
-    case Scene::FunctionMenu:
-    break;
-  }
-  currentScene = requiredToChangeScene;
-}
-
-class ServerCallbacks: public BLEMidiServerCallbacks {
-    void onConnect(BLEServer* pServer) {
-      BLEMidiServerCallbacks::onConnect(pServer);
-      changeScene(Scene::Play);
-    };
-
-    void onDisconnect(BLEServer* pServer) {
-      BLEMidiServerCallbacks::onDisconnect(pServer);
-      changeScene(Scene::Connection);
-    }
-};
 
 class MainTempoCallbacks: public TempoController::TempoCallbacks {
     void onTempoChanged(TempoController::tempo_t tempo) override {
@@ -163,10 +92,6 @@ void setup() {
   // Keymap initialization
   currentKeyMap = KeyMap::getAvailableKeyMaps()[0].get();
 
-  // Scene initialization
-  changeScene(Scene::Play);
-  _changeScene_raw();
-
   // イヤホン端子スイッチ
   pinMode(GPIO_NUM_18, INPUT_PULLUP);
 
@@ -175,13 +100,8 @@ void setup() {
   pinMode(GPIO_NUM_HOME, INPUT_PULLUP);
   pinMode(GPIO_NUM_MENU, INPUT_PULLUP);
 
-  // Midi.begin(DEVICE_NAME, new ServerCallbacks(), NULL);
   bool isHeadphone = digitalRead(GPIO_NUM_18);
   Output.Internal.begin(isHeadphone ? OutputInternal::AudioOutput::headphone : OutputInternal::AudioOutput::speaker);
-
-  Tempo.setTempo(60);
-  Tempo.addListener(new MainTempoCallbacks());
-  Tempo.start();
 }
 
 void loop() {
@@ -191,45 +111,35 @@ void loop() {
   BtnHome.setRawState(ms, digitalRead(GPIO_NUM_HOME) == 0);
   BtnMenu.setRawState(ms, digitalRead(GPIO_NUM_MENU) == 0);
 
-  switch(currentScene) {
-    case Scene::Connection:
-      if(M5.BtnA.wasPressed()) M5.Power.deepSleep();
-    break;
-    case Scene::Play:
-      if (BtnBack.wasPressed())
-      {
-        if(scale->key > 0) scale->key--;
-        else scale->key = 11;
-        M5.Lcd.clear();
-        M5.Lcd.setCursor(0, 0);
-        M5.Lcd.setTextSize(2);
-        M5.Lcd.println(scale->toString());
-      }
-      if (BtnHome.wasPressed())
-      {
-        Output.Internal.NoteOn(60 + scale->key, 100, 0);
-      }
-      else if (BtnHome.wasReleased())
-      {
-        Output.Internal.NoteOff(60 + scale->key, 0, 0);
-      }
-      if (BtnMenu.wasPressed())
-      {
-        if(scale->key < 11) scale->key++;
-        else scale->key = 0;
-        M5.Lcd.clear();
-        M5.Lcd.setCursor(0, 0);
-        M5.Lcd.setTextSize(2);
-        M5.Lcd.println(scale->toString());
-      }
+    if (BtnBack.wasPressed())
+    {
+      if(scale->key > 0) scale->key--;
+      else scale->key = 11;
+      M5.Lcd.clear();
+      M5.Lcd.setCursor(0, 0);
+      M5.Lcd.setTextSize(2);
+      M5.Lcd.println(scale->toString());
+    }
+    if (BtnHome.wasPressed())
+    {
+      Output.Internal.NoteOn(60 + scale->key, 100, 0);
+    }
+    else if (BtnHome.wasReleased())
+    {
+      Output.Internal.NoteOff(60 + scale->key, 0, 0);
+    }
+    if (BtnMenu.wasPressed())
+    {
+      if(scale->key < 11) scale->key++;
+      else scale->key = 0;
+      M5.Lcd.clear();
+      M5.Lcd.setCursor(0, 0);
+      M5.Lcd.setTextSize(2);
+      M5.Lcd.println(scale->toString());
+    }
 
-      Keypad.update();
-      currentKeyMap->update();
-    break;
-    case Scene::FunctionMenu:
-
-    break;
-  }
+    Keypad.update();
+    currentKeyMap->update();
 
   // ヘッドフォン抜き差し
   static bool isHeadphonePreviously = digitalRead(GPIO_NUM_18);
@@ -271,7 +181,6 @@ void loop() {
     *centerNoteNo = 60;
   }
 
-  if(currentScene != requiredToChangeScene) _changeScene_raw();
   while(millis() - lastLoopMillis < 33); // Keep 60fps
   lastLoopMillis = millis();
 }
