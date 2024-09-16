ebc2367d634e1adce99afa97f5f6075044d1ea60
wararyo <wararyo@gmail.com>
2024-09-16 23:24:13 +0900

    ノートフィルターを追加、コード発音時にフィルターを通過させる処理を実装

--- src/ChordPipeline.cpp
+++ src/ChordPipeline.cpp
@@ -1,38 +1,77 @@
 #include "ChordPipeline.h"
 #include "Output/MidiOutput.h"
+#include <M5Unified.h>
 
-void ChordPipeline::sendNote(bool isNoteOn, uint8_t note, uint8_t vel, uint8_t channel)
+// ノートを発音する
+// 呼び出し元がコードフィルターでない場合は、最初のフィルターに受け渡す
+// 呼び出し元がコードフィルターである場合は、次のフィルターに受け渡す
+// 次のフィルターが存在しない場合は、出力に送る
+void ChordPipeline::sendNote(bool isNoteOn, uint8_t note, uint8_t vel, uint8_t channel, NoteFilter *filter)
 {
+  // 次のフィルターのonNoteOnを呼び出す
+  // ただし次のフィルターがコードを変更しないものである場合はさらに次も呼び出す
+  for (auto itr = filter == nullptr ? noteFilters.begin() : std::next(filter->getHandler()), e = noteFilters.end(); itr != e; ++itr) {
+    if (isNoteOn) (*itr)->onNoteOn(note, vel, channel);
+    else (*itr)->onNoteOff(note, vel, channel);
+    if ((*itr)->modifiesNote()) return;
+  }
+
+  // 最後のフィルターを通過したので、出力に送る
+  if (isNoteOn) sendNoteOnRaw(note, vel, channel);
+  else sendNoteOffRaw(note, vel, channel);
+}
+
+// 複数のノートを発音する
+// 呼び出し元がコードフィルターでない場合は、最初のフィルターに受け渡す
+// 呼び出し元がコードフィルターである場合は、次のフィルターに受け渡す
+// 次のフィルターが存在しない場合は、出力に送る
+void ChordPipeline::sendNotes(bool isNoteOn, std::vector<uint8_t> notes, uint8_t vel, uint8_t channel, NoteFilter *filter)
+{
+  // 次のフィルターのonNoteOnを呼び出す
+  // ただし次のフィルターがコードを変更しないものである場合はさらに次も呼び出す
+  for (auto itr = filter == nullptr ? noteFilters.begin() : std::next(filter->getHandler()), e = noteFilters.end(); itr != e; ++itr)
+  {
+    if (isNoteOn)
+    {
+      for (uint8_t n : notes)
+      {
+        (*itr)->onNoteOn(n, vel, channel);
+      }
+    }
+    else
+    {
+      for (uint8_t n : notes)
+      {
+        (*itr)->onNoteOff(n, vel, channel);
+      }
+    }
+    if ((*itr)->modifiesNote()) return;
+  }
+
   if (isNoteOn)
   {
-    // Midi.sendNote(0x90, note, vel);
-    Output.Internal.NoteOn(note, vel, channel);
+    for (uint8_t n : notes)
+    {
+      sendNoteOnRaw(n, vel, channel);
+    }
   }
   else
   {
-    // Midi.sendNote(0x80, note, 0);
-    Output.Internal.NoteOff(note, 0, channel);
+    for (uint8_t n : notes)
+    {
+      sendNoteOffRaw(n, vel, channel);
+    }
   }
 }
 
-void ChordPipeline::sendNotes(bool isNoteOn, std::vector<uint8_t> notes, uint8_t vel, uint8_t channel)
+void ChordPipeline::sendNoteOnRaw(uint8_t note, uint8_t vel, uint8_t channel)
 {
-  if (isNoteOn)
-  {
-    for (uint8_t n : notes)
-    {
-      // Midi.sendNote(0x90, n, vel);
-      Output.Internal.NoteOn(n, vel, channel);
-    }
-  }
-  else
-  {
-    for (uint8_t n : notes)
-    {
-      // Midi.sendNote(0x80, n, 0);
-      Output.Internal.NoteOff(n, 0, channel);
-    }
-  }
+  Output.Internal.NoteOn(note, vel, channel);
+}
+
+void ChordPipeline::sendNoteOffRaw(uint8_t note, uint8_t vel, uint8_t channel)
+{
+  Output.Internal.NoteOff(note, vel, channel);
 }
 
 // コードを再生する
--- src/ChordPipeline.h
+++ src/ChordPipeline.h
@@ -12,6 +12,7 @@ public:
     class ChordFilter
     {
     public:
+        // ChordPipeline外からは使用しない
         void setHandler(std::list<ChordFilter *>::iterator handler)
         {
             this->handler = handler;
@@ -31,8 +32,32 @@ public:
         std::list<ChordFilter *>::iterator handler;
     };
 
-    void sendNote(bool isNoteOn, uint8_t note, uint8_t vel, uint8_t channel = 0);
-    void sendNotes(bool isNoteOn, std::vector<uint8_t> notes, uint8_t vel, uint8_t channel = 0);
+    // 鳴っているノートの変更を受け取ったり変更するためのフィルター
+    class NoteFilter
+    {
+    public:
+        // ChordPipeline外からは使用しない
+        void setHandler(std::list<NoteFilter *>::iterator handler)
+        {
+            this->handler = handler;
+        }
+        std::list<NoteFilter *>::iterator getHandler()
+        {
+            return handler;
+        }
+        // このフィルターがノートを変更しうるかどうか
+        // trueにする場合はフィルター側でsendNoteを呼び出す、その際にfilterを指定する
+        // falseにする場合はsendNoteを呼び出さないこと！(二重に音が鳴る)
+        virtual bool modifiesNote() { return false; }
+        // ノートが鳴り始めたときに呼び出される
+        virtual void onNoteOn(uint8_t note, uint8_t vel, uint8_t channel) = 0;
+        virtual void onNoteOff(uint8_t note, uint8_t vel, uint8_t channel) = 0;
+    private:
+        std::list<NoteFilter *>::iterator handler;
+    };
+
+    void sendNote(bool isNoteOn, uint8_t note, uint8_t vel, uint8_t channel = 0, NoteFilter *filter = nullptr);
+    void sendNotes(bool isNoteOn, std::vector<uint8_t> notes, uint8_t vel, uint8_t channel = 0, NoteFilter *filter = nullptr);
     void playChord(Chord chord, ChordFilter *filter = nullptr);
     void stopChord(ChordFilter *filter = nullptr);
     void addChordFilter(ChordFilter *filter)
@@ -43,10 +68,22 @@ public:
     {
         chordFilters.remove(filter);
     }
+    void addNoteFilter(NoteFilter *filter)
+    {
+        filter->setHandler(noteFilters.insert(noteFilters.end(), filter));
+    }
+    void removeNoteFilter(NoteFilter *filter)
+    {
+        noteFilters.remove(filter);
+    }
 
 private:
     std::list<ChordFilter *> chordFilters;
+    std::list<NoteFilter *> noteFilters;
     std::vector<uint8_t> playingNotes;
+
+    void sendNoteOnRaw(uint8_t note, uint8_t vel, uint8_t channel);
+    void sendNoteOffRaw(uint8_t note, uint8_t vel, uint8_t channel);
 };
 
 extern ChordPipeline Pipeline;
