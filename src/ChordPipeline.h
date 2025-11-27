f427f27eaf92d9262ef07bec1f649c2e65de5a96
wararyo <wararyo@gmail.com>
2025-11-27 22:26:49 +0900

    コードフィルターとノートフィルターにおけるスレッドセーフティを向上

--- src/ChordPipeline.h
+++ src/ChordPipeline.h
@@ -2,12 +2,17 @@
 #include <cstdint>
 #include <vector>
 #include <list>
+#include <freertos/FreeRTOS.h>
+#include <freertos/semphr.h>
 #include "Chord.h"
 
 // キーを押してからMIDIを出力するまでの一連の動作を管理するクラス
 class ChordPipeline
 {
 public:
+    ChordPipeline() {
+        filterMutex = xSemaphoreCreateMutex();
+    }
     // 鳴っているコードの変更を受け取ったり変更するためのフィルター
     class ChordFilter
     {
@@ -62,25 +67,34 @@ public:
     void stopChord(ChordFilter *filter = nullptr);
     void addChordFilter(ChordFilter *filter)
     {
+        if (filterMutex) xSemaphoreTake(filterMutex, portMAX_DELAY);
         filter->setHandler(chordFilters.insert(chordFilters.end(), filter));
+        if (filterMutex) xSemaphoreGive(filterMutex);
     }
     void removeChordFilter(ChordFilter *filter)
     {
+        if (filterMutex) xSemaphoreTake(filterMutex, portMAX_DELAY);
         chordFilters.remove(filter);
+        if (filterMutex) xSemaphoreGive(filterMutex);
     }
     void addNoteFilter(NoteFilter *filter)
     {
+        if (filterMutex) xSemaphoreTake(filterMutex, portMAX_DELAY);
         filter->setHandler(noteFilters.insert(noteFilters.end(), filter));
+        if (filterMutex) xSemaphoreGive(filterMutex);
     }
     void removeNoteFilter(NoteFilter *filter)
     {
+        if (filterMutex) xSemaphoreTake(filterMutex, portMAX_DELAY);
         noteFilters.remove(filter);
+        if (filterMutex) xSemaphoreGive(filterMutex);
     }
 
 private:
     std::list<ChordFilter *> chordFilters;
     std::list<NoteFilter *> noteFilters;
     std::vector<uint8_t> playingNotes;
+    SemaphoreHandle_t filterMutex = nullptr;
 
     void sendNoteOnRaw(uint8_t note, uint8_t vel, uint8_t channel);
     void sendNoteOffRaw(uint8_t note, uint8_t vel, uint8_t channel);
